d7728a9a67d34ce2c0d91df2e56d680b49264a0e
ext-yuren.zhang <ext-yuren.zhang@le.com>
2018-04-10 18:41:01 +0800

    pc7

--- djweb1/cmdb/models.py
+++ djweb1/cmdb/models.py
@@ -1,6 +1,31 @@
 from django.db import models
-
 # Create your models here.
-class UserInfo(models.Model):
-    user = models.CharField(max_length=32)
-    pwd = models.CharField(max_length=32)
+# class UserInfo(models.Model):
+#     user = models.CharField(max_length=32)
+#     pwd = models.CharField(max_length=32)
+
+
+# class Employeee(models.Model):
+#     name = models.CharField(max_length=20)
+'''
+django 1.9之后
+
+python manage.py syncdb
+
+改成了
+python manage.py makemigrations
+python manage.py migrate
+
+
+或者
+python manage.py makemigrations
+
+'''
+
+
+
+
+from django.db import models
+
+class Employee(models.Model):
+     name=models.CharField(max_length=20)
--- djweb1/djweb1/settings.py
+++ djweb1/djweb1/settings.py
@@ -74,23 +74,23 @@ WSGI_APPLICATION = 'djweb1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'mysql',  # 连接数据库的名字
-#         'USER': 'root',   # 数据库用户名字
-#         'PASSWORD': 'new_pass', # 数据库密码
-#         'HOST': '127.0.0.1',
-#         'PORT': '3306',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pydata',  # 连接数据库的名字
+        'USER': 'root',   # 数据库用户名字
+        'PASSWORD': 'new_pass',  # 数据库密码
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
 
 
 # Password validation
